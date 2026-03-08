d9e3d17ed7862addcff2de112ba680ea336c3e55
Romain Bnft <romain.bonnifet.work@gmail.com>
2026-03-08 18:08:07 +0100

    add: modules

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+const express = require('express')
+const morgan = require('morgan')
+const favicon = require("serve-favicon")
+const bodyParser = require('body-parser')
+const sequelize = require('./src/db/sequelize')
+
+const app = express()
+const port = 3000
+
+
+
+app.use(favicon(__dirname + '/favicon.ico')).use(morgan('dev')).use(bodyParser.json())
+
+sequelize.initDb()
+
+require('./src/routes/findAllPokemons')(app)
+require('./src/routes/findPokemonByPk')(app)
+require('./src/routes/createPokemon')(app)
+require('./src/routes/updatePokemon')(app)
+require('./src/routes/deletePokemon')(app)
+
+app.listen(port, ()=> console.log(`Appli Node démarrée. Adresse : http://localhost:${port}`));
